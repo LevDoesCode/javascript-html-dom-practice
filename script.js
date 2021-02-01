f1a6c307fb97c15e6aa651d148290f155f3b87b3
Lev <levdoescode@gmail.com>
2021-02-01 14:29:04 -0500

    Update click events

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let dem;
-dem = document.getElementById('demo');
-if( dem != undefined )
-{
-    dem.innerHTML = "Hello World";
+
+let button = document.getElementById("btn");
+
+function whichButton(e){
+    if(e.button == 0){
+        alert("Left Button");
+    }
+    else if (e.button == 1){
+        alert("Middle Button");
+    }
+    else if (e.button == 2){
+        alert("Right Button");
+    }
+    console.log(e);
 }
+button.addEventListener("mousedown", whichButton);

b0dc130924985031f2b83be2c43843c98eaa8aba
Lev <levdoescode@gmail.com>
2021-02-01 15:28:06 -0500

    Add mouse move events

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let dem;
-dem = document.getElementById('demo');
-if( dem != undefined )
-{
-    dem.innerHTML = "Hello World";
+let lastX;
+let item = document.getElementById("item");
+item.addEventListener("mousedown", function(e){
+    if(e.button == 0)
+    {
+        lastX = e.clientX;
+        window.addEventListener("mousemove", moved);
+    }
+})
+
+function moved(e){
+    let dist = e.clientX - lastX
+    let newWidth = item.offsetWidth + dist;
+    item.style.width = newWidth + "px";
+    lastX = e.clientX;
 }

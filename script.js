d4e75ca586749c8946fb48851ae8c1774917a99f
Lev <levdoescode@gmail.com>
2021-02-01 12:47:29 -0500

    Update event listeners

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ if( dem != undefined )
 {
     dem.innerHTML = "Hello World";
 }
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", once);
+function once(){
+    document.getElementById("btn").textContent = "You clicked the button";
+    // btn.textContent = "You clicked the button";
+}
+
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", one);
+btn2.addEventListener("mouseout", two);
+
+function one(){
+    document.getElementById("btn2").textContent
+     = "Clicked Button 2";
+}
+
+function two(){
+    alert(this.id);
+}
